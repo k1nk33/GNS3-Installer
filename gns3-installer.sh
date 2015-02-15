e8d5c478389e1e7230f047aa09cc67e46587e82a
Darren <k1nk33.dd@gmail.com>
2015-02-15 19:12:24 +0000

    Final Sunday Commit

--- gns3-installer.sh
+++ gns3-installer.sh
@@ -7,12 +7,12 @@
 
 # As of this time only Ubunutu 14.04 ( or derivatives ) are accepted.
 
+# Set the start time
+start=$(date +%s)
+
 ###################################################################
 ###################### Check Requirements #########################
 ###################################################################
-
-# Set the start time
-start=$(date +%s)
 
 distro=$(lsb_release -a 2>/dev/null | grep Description | cut -f2)
 echo $distro
@@ -20,12 +20,12 @@ echo $distro
 if [ "$distro" != "Ubuntu 14.04.1 LTS" ];then ###&& [ "$distro" != "Ubuntu 12.04.5 LTS" ];then
 	echo -e "\e[31m********* Error, Wrong Distro **********\e[0m "
 	sleep 1
+	echo 
+	echo -e "\e[31m****** Quitting script! ******\e[0m "
 	echo
 	end=$(date +%s)
 	runtime=$(($end-$start))
 	echo -e "\e[32m****** Total Runtime is "$runtime" sec's ******\e[0m"
-	echo
-	echo -e "\e[31m****** Quitting script! ******\e[0m "
 	exit 0
 fi
 
@@ -45,11 +45,9 @@ cd /tmp/gns3
 
 # Download the required files
 echo -e "\e[31mDownloading Files!\e[0m "
-sleep 2
 wget http://downloads.gns3.com/GNS3-1.2.3.source.zip -O gns.zip
 echo
 echo -e "\e[31mInitial download complete\e[0m"
-sleep 2
 echo
 
 # Extract the main archive
@@ -63,8 +61,7 @@ unzip "*.zip"
 rm *.zip
 
 # At this point the files are extracted to their respective folders
-
-echo
+# Request permission to continue installing packages
 echo -e "\e[31mThe script needs to install dependencies\e[0m "
 echo
 echo -en "\e[31mEnter\e[0m \e[32myes\e[0m \e[31mto\e[0m \e[32mcontinue\e[0m \e[31mor\e[0m \e[32mno\e[0m \e[31mto\e[0m \e[32mquit : \e[0m "
@@ -80,7 +77,7 @@ while true; do
 		read response
 	# Qtherwise the entry is acceptable
 	else
-		break
+		break		
 	fi
 done
 
@@ -88,28 +85,22 @@ done
 ###################### Install Dependencies #######################
 ###################################################################
 
-# Seriously now, why do I need to refer to response after the or?
+# Seriously now, why do I need to refer to response after the or 
 if [ "$response" = "no" -o "$response" = "NO" ];then
 	echo -e "\e[31m****** Quitting!!! ******\e[0m "
 	cd
 	rm -R /tmp/gns3/GNS3-1.2.3.source
 	echo -e "\e[31mFolder removed\e[0m "
-	end=$(date +%s)
-	runtime=$(($end-$start))
-	echo "Total Runtime is "$runtime" sec's"
 	exit 0
 else
 	echo -e "\e[31mInstalling Packages, please be patient!\e[0m "
 	# Make sure to run apt-get update !
 	sudo apt-get update
-	# Default Y/n prompt to "yes" with y operand
-	sudo apt-get -y install python3 libpcap-dev uuid-dev libelf-dev cmake python3-setuptools python3-pyqt4 python3-ws4py python3-zmq python3-tornado
-	# some sort of problem installing python3-netifaces package
-	# needs to be installed separate
-	sudo apt-get -y install python3-netifaces
+	# Install requirements, use Y switch to default to yes at prompt!
+	sudo apt-get install -y python3 libpcap-dev uuid-dev libelf-dev cmake python3-setuptools python3-pyqt4 python3-ws4py python3-zmq python3-tornado
+	sudo apt-get install -y python3-netifaces
 	echo
 	echo -e "\e[32m****** All done installing packages ******\e[0m "
-	sleep 2
 	echo
 fi
 
@@ -117,134 +108,148 @@ fi
 ######################## Installation Stage #######################
 ###################################################################
 
-# Dynamips install
-cd dynamips*
-mkdir build ; cd build
-cmake ..
-make
-echo -e "\e[31m****** Installing Dynamips ******\e[0m "
-sleep 1
-sudo make install
-echo
-echo -e "\e[32m****** Dynamips installed ******\e[0m "
-sleep 2
-echo
+# Dynamips installer function
+function dynamips_install
+{
+	cd /tmp/gns3/GNS3*.source/dynamips*
+	mkdir build ; cd build
+	cmake ..
+	make
+	echo -e "\e[31m****** Installing Dynamips ******\e[0m "
+	sudo make install
+	echo
+	echo -e "\e[32m****** Dynamips installed ******\e[0m "
+	echo
+}
 
-# Server install
-cd ..
-cd ../gns3-server*
-echo -e "\e[31m****** Installing GNS Server ******\e[0m "
-sleep 1
-sudo python3 setup.py install
-echo
-echo -e "\e[32m****** GNS3 Server installed ******\e[0m "
-sleep 2
+# GNS3 Server installer function
+function gns3_srv_install
+{
+	cd /tmp/gns3/GNS3*.source/gns3-server*
+	echo -e "\e[31m****** Installing GNS Server ******\e[0m "
+	sudo python3 setup.py install
+	echo
+	echo -e "\e[32m****** GNS3 Server installed ******\e[0m "
+}
 
-# GUI install
-cd ../gns3-gui*
-echo
-echo -e "\e[31m****** Installing GNS GUI ******\e[0m "
-sleep 1
-sudo python3 setup.py install
-echo
-echo -e "\e[32m****** GNS3 GUI installed ******\e[0m "
-sleep 2
-echo
+# GUI install function
+function gns3_gui_install
+{
+	cd /tmp/gns3/GNS3*.source/gns3-gui*
+	echo
+	echo -e "\e[31m****** Installing GNS GUI ******\e[0m "
+	sudo python3 setup.py install
+	echo
+	echo -e "\e[32m****** GNS3 GUI installed ******\e[0m "
+	echo
+	# Create the desktop icon
+	create_icon
+}
 
-# VPCS Install
-cd ../vpcs*/src
-echo -e "\e[31m****** Attempting to install VPCS ******\e[0m "
-echo
-sleep 1
-if [ "$architecture" = "x86_64" ];then
-	echo -e "\e[34m****** 64 Bit Install ******\e[0m "
-	sleep 1
-	echo $(sudo ./mk.sh 64)
-else
-	echo -e "\e[34m****** 32 Bit Install ******\e[0m "
-	sleep 1
-	echo $(sudo ./mk.sh 32 )
-fi
+# VPCS Installer function
+function vpcs_install
+{
+	cd /tmp/gns3/GNS3*.source/vpcs*/src
+	echo -e "\e[31m****** Attempting to install VPCS ******\e[0m "
+	echo
+	if [ "$architecture" = "x86_64" ];then
+		echo -e "\e[34m****** 64 Bit Install ******\e[0m "
+		echo $(sudo ./mk.sh 64)
+	else
+		echo -e "\e[34m****** 32 Bit Install ******\e[0m "
+		echo $(sudo ./mk.sh 32 )
+	fi
 
-# Copy the binary to /usr/bin
-sudo cp vpcs /usr/bin
-echo -e "\e[32m****** Virtual PC is installed ******\e[0m "
-echo -e "\e[32m*** You can execute it from terminal by typing 'vpcs' and hitting enter ***\e[0m "
-sleep 5
-echo
+	# Copy the binary to /usr/bin
+	sudo cp vpcs /usr/bin
+	echo -e "\e[32m****** Virtual PC is installed ******\e[0m "
+	echo -e "\e[32m*** You can execute it from terminal by typing 'vpcs' and hitting enter ***\e[0m "
+	echo
+}
 
-# Create the application icon for GNS3
-text="[Desktop Entry]\n
-Name=GNS3\n
-Comment=Graphical Network Simulator\n
-Keywords=network;simulator\n
-Exec=/usr/local/bin/gns3\n
-Icon=/usr/share/applications/gns3.png\n
-Terminal=false\n
-Type=Application\n
-StartupNotify=true\n
-Categories=Development"
+# Function to create the desktop icon for gns3
+function create_icon
+{
+	text="[Desktop Entry]\n
+	Name=GNS3\n
+	Comment=Graphical Network Simulator\n
+	Keywords=network;simulator\n
+	Exec=/usr/local/bin/gns3\n
+	Icon=/usr/share/applications/gns3.png\n
+	Terminal=false\n
+	Type=Application\n
+	StartupNotify=true\n
+	Categories=Development"
 
-echo -e "\e[31m****** Creating an Application Icon for GNS3 ******\e[0m "
-sudo cp $working_directory/gns3.png /usr/share/applications
-echo -e $text > $working_directory/gns3.desktop && sudo mv $working_directory/gns3.desktop /usr/share/applications
-# Give the file the proper permissions
-sudo chmod u+x /usr/share/applications/gns3.desktop
-sleep 2
-echo -e "\e[32m****** All Done ******\e[0m "
-echo
-sleep 1
+	echo -e "\e[31m****** Creating an Application Icon for GNS3 ******\e[0m "
+	sudo cp $working_directory/gns3.png /usr/share/applications
+	echo -e $text > $working_directory/gns3.desktop && sudo mv $working_directory/gns3.desktop /usr/share/applications
+	# Give the file the proper permissions
+	sudo chmod u+x /usr/share/applications/gns3.desktop
+	echo -e "\e[32m****** All Done ******\e[0m "
+	echo
+}
 
-# Clean up
-cd
-echo -e "\e[31m****** House Cleaning! ******\e[0m " 
-sudo rm -R /tmp/gns3
-sleep 1
-echo -e "\e[32m****** Folder removed ******\e[0m " 
-echo
+# Clean up function
+function clean_up
+{
+	cd
+	echo -e "\e[31m****** House Cleaning! ******\e[0m " 
+	sudo rm -R /tmp/gns3
+}
 
 ###################################################################
 ######## Check that all software installed successfully ###########
 ###################################################################
 
-# First time bash function!
 # Detects if the software has been installed correctly.
-function sw_check {
+function sw_check
+{
 	gns=$(which gns3)
 	vpc=$(which vpcs)
 	dmips=$(which dynamips)
-	
-	# As long as the variables return a value other than empty, the programs have installed
-	if [ -n "$gns" ];then
-		echo -e "\e[32m****** GNS3 Install Successful! ******\e[0m ";echo
-		if [ -n "$vpc" ];then
-			echo -e "\e[32m****** VPCS Install Successful! ******\e[0m ";echo
-			if [ -n "$dmips" ];then
-				echo -e "\e[32m****** Dynamips Install Successful! ******\e[0m ";echo
+	while true; do
+		# As long as the variables return a value other than empty, the programs have installed
+		if [ -n "$gns" ];then
+			echo -e "\e[32m****** GNS3 Install Successful! ******\e[0m ";echo
+			if [ -n "$vpc" ];then
+				echo -e "\e[32m****** VPCS Install Successful! ******\e[0m ";echo
+				if [ -n "$dmips" ];then
+					echo -e "\e[32m****** Dynamips Install Successful! ******\e[0m ";echo
+				else
+					echo -e "\e[31mError, dynamips value = $dmips\e[0m "
+				fi
 			else
-				echo -e "\e[31mError, dynamips value = $dmips\e[0m "
+				echo -e "\e[31mError, vpcs value = $vpc\e[0m "
 			fi
 		else
-			echo -e "\e[31mError, vpcs value = $vpc\e[0m "
+			echo -e "\e[31mError, gns value = $gns\e[0m "
 		fi
-	else
-		echo -e "\e[31mError, gns value = $gns\e[0m "
-	fi
+		break
+	done
 }
-
-# Call the function to determine if the software was installed
-sw_check
 
 ###################################################################
 ######################## Exiting script ###########################
 ###################################################################
-echo
-echo -e "\e[31m****** Don't forget to remove the Working Directory as it is no longer needed! ******\e[0m "
-echo
-end=$(date +%s)
-runtime=$(($end-$start))
-echo -e "\e[32m****** Total Runtime is "$runtime" sec's ******\e[0m"
-echo
-echo -e "\e[31m****** Exiting from the script! ******\e[0m "
-sleep 2
-exit 0
+
+function script_exit
+{
+	echo -e "\e[31m*** Don't forget to remove the Working Directory as it is no longer needed! ***\e[0m"
+	echo
+	end=$(date +%s)
+	runtime=$(($end-$start))
+	echo -e "\e[32m****** Total Runtime is "$runtime" sec's ******\e[0m"
+	echo
+	echo -e "\e[31m****** Exiting from the script! ******\e[0m "
+	exit 0
+}
+
+# Function calls
+dynamips_install 
+vpcs_install & # fork
+gns3_srv_install
+gns3_gui_install
+clean_up & # fork
+sw_check
+script_exit
